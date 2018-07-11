276a52b8283c5ee2d7b5614952e5ee2670fcd913
Mwelgama <hmwelgam@deakin.edu.au>
2018-07-11 18:46:51 +1000

    NEW: Add Device List, Register Device, Send Command html pages. Also added JavaScript support

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+// const devices = [];
+
+for (let key of devices) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${key.User}</td>
+    <td>${key.Name}</td>
+    </tr>`);   
+};
+
+$('#add-device').on('click', function(){
+    const user1 = $('#user').val();
+    const name1 = $('#name').val()
+    devices.push({User: user1, Name: name1})
+    localStorage.setItem('devices', JSON.stringify(devices));
+    console.log(devices);
+    location.href = "device-list.html"
+});
+
+$('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+   });
+   
+
+
