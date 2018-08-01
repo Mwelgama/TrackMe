dc2d5702da1a49d229b2964f20b584edf532b676
Mwelgama <hmwelgam@deakin.edu.au>
2018-08-01 13:53:52 +1000

    Testing2

--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const base = `${__dirname}/Public`;
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.get('/', (req, res) => {
     res.sendFile(`${base}/device-list.html`);
